19525dfdcbb1ec131b2f163985a2b9fc12a4181b
StevenVallarsa <orionsjv@gmail.com>
2022-01-05 18:15:42 -0500

    added scanner

--- src/main/java/com/sv/interestcalculator/InterestCalculator.java
+++ src/main/java/com/sv/interestcalculator/InterestCalculator.java
@@ -5,13 +5,46 @@
 
 package com.sv.interestcalculator;
 
+import java.util.Scanner;
+
 /**
  *
  * @author: Steven Vallarsa
  *   email: stevenvallarsa@gmail.com
  *    date: 2022-00-00
- * purpose: 
+ * purpose: A simple interest calculator
  */
 public class InterestCalculator {
-
+    
+    public static void main(String[] args) {
+        // get deposit
+        // get 
+        
+    }
+    
+    
+    private static double getInput(String prompt) {
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        double number = 0;
+        
+        while (true) {
+            System.out.println(prompt);
+            
+            String input = scanner.nextLine();
+            
+            try {
+                number = Double.parseDouble(input);
+                if (number < 1 || number > 25) {
+                    throw new Exception();
+                }
+                return number;
+            } catch (NumberFormatException e) {
+                System.out.println("That wasn't a number. Try again.\n");
+            } catch (Exception e) {
+                System.out.println("That number wasn't valid. Try again. \n");
+            }
+        }
+    }
 }
