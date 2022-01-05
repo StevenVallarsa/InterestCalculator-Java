1952a8ea608feb0e872ba2a6e1c4fa32f78f2c5f
StevenVallarsa <orionsjv@gmail.com>
2022-01-05 17:48:58 -0500

    first commit

---
+++ src/main/java/com/sv/interestcalculator/InterestCalculator.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package com.sv.interestcalculator;
+
+/**
+ *
+ * @author: Steven Vallarsa
+ *   email: stevenvallarsa@gmail.com
+ *    date: 2022-00-00
+ * purpose: 
+ */
+public class InterestCalculator {
+
+}
